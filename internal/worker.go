dfc5f1fc69f4ca8a0cdf7204cea16b0e1f13e5f1
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-07 18:30:24 +0430

    update: worker

--- internal/worker.go
+++ internal/worker.go
@@ -5,28 +5,41 @@ import (
 	"net"
 )
 
+// worker handles a single client that wants
+// to either subscribe or publish.
 type worker struct {
+	id   int
+	http http
+
 	sendChannel    chan []byte
 	receiveChannel chan []byte
-
-	http http
-	id   int
 }
 
-func NewWorker(id int, conn net.Conn) *worker {
+// NewWorker generates a new worker.
+// id: for worker id.
+// conn: http connection over TCP.
+// sen: sending channel.
+// rec: receive channel.
+func NewWorker(id int, conn net.Conn, sen, rec chan []byte) *worker {
 	return &worker{
+		id: id,
 		http: http{
 			conn: conn,
 		},
-		id: id,
+		receiveChannel: rec,
+		sendChannel:    sen,
 	}
 }
 
+// Start will start our worker.
 func (w *worker) Start() {
+	// close send connection
 	defer close(w.sendChannel)
 
+	// start for input data
 	go w.receive()
 
+	// wait for any interrupt in send channel
 	select {
 	case data := <-w.sendChannel:
 		w.send(data)
