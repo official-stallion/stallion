3aa511749b93c0fb69ede7e3e2a432bb6cd22479
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-09 12:56:11 +0430

    add: message type

--- internal/types.go
+++ internal/types.go
@@ -3,6 +3,12 @@ package internal
 // MessageHandler is a handler for messages that come from subscribing.
 type MessageHandler func([]byte)
 
+// Message is what we send between worker and clients.
+type Message struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+}
+
 // workChan is worker channel with its id.
 type workChan struct {
 	id      int
