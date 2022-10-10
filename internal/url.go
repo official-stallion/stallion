0a2fc36e7a52c017b1bb689a049bf412d9779797
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-10 16:32:11 +0330

    add: host and port

--- internal/url.go
+++ internal/url.go
@@ -17,15 +17,18 @@ type url struct {
 // urlUnpack
 // manages to create url struct from url string.
 func urlUnpack(inputUrl string) (*url, error) {
+	// check the input url protocol
 	protocolSplit := strings.Split(inputUrl, "://")
 	if len(protocolSplit) < 2 {
 		return nil, fmt.Errorf("invalid uri")
 	}
 
+	// protocol must be 'st'
 	if protocolSplit[0] != "st" {
 		return nil, fmt.Errorf("not using stallion protocol (st://...)")
 	}
 
+	// exporting the host:port pair.
 	hostInformation := strings.Split(protocolSplit[1], ":")
 	if len(hostInformation) < 2 {
 		return nil, fmt.Errorf("server ip or port is not given")
