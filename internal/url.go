41b8b95f1f5c72c5787b2bcd1bb5ea4e3d0920f2
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-10 16:22:24 +0330

    refactor: url method

--- internal/url.go
+++ internal/url.go
@@ -1,1 +1,16 @@
 package internal
+
+// url
+// each url contains of the following parts:
+// - host
+// - port
+type url struct {
+	host string
+	port string
+}
+
+// urlUnpack
+// manages to create url struct from url string.
+func urlUnpack(url string) *url {
+	return nil
+}
