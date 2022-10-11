b3cf7343c3df0dcdefc5c1d357ca4e3ca019a27f
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-11 14:02:32 +0330

    update: client auth example

---
+++ example/client/auth-client/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/official-stallion/stallion"
+)
+
+func main() {
+	client, err := stallion.NewClient("st://root:Pa$$word@localhost:9090")
+	if err != nil {
+		panic(err)
+	}
+
+	client.Subscribe("topic", func(data []byte) {
+		fmt.Println(string(data))
+	})
+
+	client.Publish("topic", []byte("Hello"))
+
+	time.Sleep(3 * time.Second)
+}
