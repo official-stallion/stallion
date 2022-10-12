c48b516ecc38ddb0b4386d2bdc4142f20dcefefe
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-12 13:00:24 +0330

    implement: mock methods

--- mock.go
+++ mock.go
@@ -1,21 +1,35 @@
 package stallion
 
-import "github.com/official-stallion/stallion/internal"
+import (
+	"fmt"
 
-type mockClient struct{}
+	"github.com/official-stallion/stallion/internal"
+)
+
+type mockClient struct {
+	topics map[string]internal.MessageHandler
+}
 
 func NewMockClient() Client {
-	return &mockClient{}
+	return &mockClient{
+		topics: make(map[string]internal.MessageHandler),
+	}
 }
 
 func (m *mockClient) Publish(topic string, data []byte) error {
+	if handler, ok := m.topics[topic]; ok {
+		handler(data)
 
+		return nil
+	}
+
+	return fmt.Errorf("failed to publish")
 }
 
 func (m *mockClient) Subscribe(topic string, handler internal.MessageHandler) {
-
+	m.topics[topic] = handler
 }
 
 func (m *mockClient) Unsubscribe(topic string) {
-
+	delete(m.topics, topic)
 }
