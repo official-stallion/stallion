f37d235b6db2f514fd61c66f29d20351a725752d
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-12 12:57:52 +0330

    add: mock client

--- mock.go
+++ mock.go
@@ -1,11 +1,21 @@
 package stallion
 
-type MockClient struct{}
+import "github.com/official-stallion/stallion/internal"
 
-func NewMockClient() MockClient {
+type mockClient struct{}
+
+func NewMockClient() Client {
+	return &mockClient{}
+}
+
+func (m *mockClient) Publish(topic string, data []byte) error {
 
 }
 
-func (m *MockClient) Publish() {
+func (m *mockClient) Subscribe(topic string, handler internal.MessageHandler) {
+
+}
+
+func (m *mockClient) Unsubscribe(topic string) {
 
 }
