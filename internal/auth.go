f473aa28302f4f90a78d618bdc4c6fc08af8fdea
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-15 09:35:57 +0330

    implement: authenticate method

--- internal/auth.go
+++ internal/auth.go
@@ -1,1 +1,14 @@
 package internal
+
+import "strings"
+
+type auth struct {
+	username string
+	password string
+}
+
+func (a *auth) authenticate(token string) bool {
+	parts := strings.Split(token, ":")
+
+	return parts[0] == a.username && parts[1] == a.password
+}
