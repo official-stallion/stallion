0b96b225c5de4700c7ba8c3c1c92cb6965885cdc
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-15 09:36:48 +0330

    add: auth type

--- internal/auth.go
+++ internal/auth.go
@@ -2,11 +2,15 @@ package internal
 
 import "strings"
 
+// auth
+// manages to keep authentication data.
 type auth struct {
 	username string
 	password string
 }
 
+// authenticate
+// checks the user authentication.
 func (a *auth) authenticate(token string) bool {
 	parts := strings.Split(token, ":")
 
