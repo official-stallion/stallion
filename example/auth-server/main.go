a9a2ce1ec79671d3aa21331fee65e8f5a007f006
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-23 09:14:48 +0330

    update: examples

--- example/auth-server/main.go
+++ example/auth-server/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/official-stallion/stallion"
 
 func main() {
-	if err := stallion.NewServer(":9090", "root", "Pa$$word"); err != nil {
+	if err := stallion.NewServer(":9090", 9091, "root", "Pa$$word"); err != nil {
 		panic(err)
 	}
 }
--- example/simple-server/main.go
+++ example/simple-server/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/official-stallion/stallion"
 
 func main() {
-	if err := stallion.NewServer(":9090"); err != nil {
+	if err := stallion.NewServer(":9090", 9091); err != nil {
 		panic(err)
 	}
 }
